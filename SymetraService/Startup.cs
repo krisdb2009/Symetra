3470b05f016e7d03d9c2ee941aa8582b13b6e25d
Dylan K. Bickerstaff <dylan@belowaverage.org>
2020-12-20 16:18:16 -0500

    Add project files.

---
+++ SymetraService/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SymetraService.Startup))]
+
+namespace SymetraService
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR("/signalr", new HubConfiguration());
+        }
+    }
+}
---
+++ SymetraService/Symetra.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Timers;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SymetraService
+{
+    public class Symetra : Hub
+    {
+        public Symetra()
+        {
+            Debug.WriteLine("New connection.");
+        }
+    }
+}
